e9aca6516da36c301ebab42d05e9bf1c44ad7dc9
Alisson00 <alissonpzz889@gmail.com>
2025-11-22 15:00:07 -0300

    niveis de dificuldade(fácil, médio e dificil)

--- Trivia.py
+++ Trivia.py
@@ -6,3 +6,7 @@ print("2 - Médio")
 print("3 - Difícil")
 
 dificuldade = input("Digite o número da dificuldade: ")
+
+if dificuldade not in ["1", "2", "3"]:
+    print("Dificuldade inválida. Vamos jogar no nível Fácil!\n")
+    dificuldade = "1"
